f3e37197a095259c591106e73b5334d9d8cb9b6e
unknown <vasanthgamer2000@gmail.com>
2023-07-28 17:21:05 +0530

    Changes have been committed

--- Program.cs
+++ Program.cs
@@ -10,6 +10,50 @@ namespace Phase1Section4._25
     {
         static void Main(string[] args)
         {
+            //Insertion Sort
+            runApp();
+            Console.ReadKey();
         }
+
+        public static void runApp()
+        {
+            string[] students = new string[10];
+            students[0] = "Karuna";
+            students[1] = "Mark";
+            students[2] = "Advaith";
+            students[3] = "Sangeeta";
+            students[4] = "Nazia";
+            students[5] = "Faisal";
+            students[6] = "Tania";
+            students[7] = "Guru";
+            students[8] = "Chandni";
+            students[9] = "Kamleshwar";
+
+            int n = 10, i, j, flag;
+            string val;
+            for (i = 1; i < n; i++)
+            {
+                val = students[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val.CompareTo(students[j]) < 0)
+                    {
+                        students[j + 1] = students[j];
+                        j--;
+                        students[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+
+
+            }
+
+            foreach (string s in students)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
     }
 }
